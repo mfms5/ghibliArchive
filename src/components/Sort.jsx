7949559ede5f63ca9a7d4b3d765a18a413587583
Marta FMS <32145806+mfms5@users.noreply.github.com>
2025-12-03 17:27:55 +0100

    Custom dropdown for sorting button

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,15 +1,75 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleUp, faAngleDown } from "@fortawesome/free-solid-svg-icons";
 
 const Sort = ({ movieListRef }) => {
-  const [sortingType, setSortingType] = useState("yearAsc");
+  const [isOpen, setIsOpen] = useState(false);
+  const [sortingTypeValue, setSortingTypeValue] = useState("yearAsc");
+  const [sortingTypeLabel, setSortingTypeLabel] = useState("Year ↑");
 
-  const onChangeSortType = (type) => {
-    setSortingType(type);
+  const types = [
+    { name: "Year ↑", value: "yearAsc" },
+    { name: "Year ↓", value: "yearDes" },
+    { name: "Rating ↑", value: "ratingAsc" },
+    { name: "Rating ↓", value: "ratingDes" },
+  ];
+
+  const onChangeSortType = (type, name) => {
+    setSortingTypeValue(type);
+    setSortingTypeLabel(name);
     movieListRef.current.setSortingType(type);
+    setIsOpen(false);
   };
 
+  const close = useCallback(() => {
+    setIsOpen(false);
+  }, []);
+
+  //Allow for closing the dropdown by clicking outside
+  useEffect(() => {
+    setTimeout(() => {
+      if (isOpen) {
+        window.addEventListener("click", close);
+      } else {
+        window.removeEventListener("click", close);
+      }
+    }, 0);
+  }, [isOpen]);
+
   return (
-    <label>
+    <div className="dropdown-wrapper">
+      <span className="h3 text-primary font-semibold text-xl">Sort by: </span>
+      <div>
+        <button
+          type="button"
+          className="dropdown-header"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <div className="dropdown-header-title">{sortingTypeLabel}</div>
+          {isOpen ? (
+            <FontAwesomeIcon icon={faAngleUp} />
+          ) : (
+            <FontAwesomeIcon icon={faAngleDown} />
+          )}
+        </button>
+        {isOpen && (
+          <div className="dropdown-list">
+            {types.map((type) => (
+              <button
+                type="button"
+                className="dropdown-item"
+                key={type.value}
+                onClick={() => onChangeSortType(type.value, type.name)}
+              >
+                {type.name}
+              </button>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+
+    /*<label>
       Sort by:
       <select
         name="sortingType"
@@ -21,7 +81,7 @@ const Sort = ({ movieListRef }) => {
         <option value="ratingAsc">Rating ↑</option>
         <option value="ratingDes">Rating ↓</option>
       </select>
-    </label>
+    </label>*/
   );
 };
 
