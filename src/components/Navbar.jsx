456fec6e8eec576f557fbcbed15b0fb4f9e20661
Marta FMS <32145806+mfms5@users.noreply.github.com>
2025-12-04 12:28:41 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="navbar">
-      <Link to="/" className="flex flex-row items-center">
+      <Link to="/" className="">
         <img src="home.png" alt="home" className="left-2" />
         Home
       </Link>
