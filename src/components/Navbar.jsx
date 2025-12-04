edf4630a5d14f50cfb29282b87e35bcae1129588
Marta FMS <32145806+mfms5@users.noreply.github.com>
2025-12-04 12:28:05 +0100

    Home and About icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,14 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="navbar">
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
+      <Link to="/" className="flex flex-row items-center">
+        <img src="home.png" alt="home" className="left-2" />
+        Home
+      </Link>
+      <Link to="/about">
+        <img src="info.png" alt="about" className="left-2" />
+        About
+      </Link>
     </div>
   );
 };
